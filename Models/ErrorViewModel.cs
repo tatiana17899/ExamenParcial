e9d405aa6bb8590f6d4d40e91111c60ff5cc39ff
tatiana17899 <tatianasuarezrosas332@gmail.com>
2024-10-04 11:01:40 -0500

    mvc identity

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExamenParcial.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
